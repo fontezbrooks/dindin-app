9627e6ec2f0e477189a80329604be45f35d3e9e9
fontezbrooks <fontez.brooks@cinemassive.com>
2025-10-28 00:38:26 -0400

    actual working card demo

---
+++ apps/frontend/app/(tabs)/index.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+
+export default function TabOneScreen() {
+  return (
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Tab One</Text>
+        <View
+          darkColor="rgba(255,255,255,0.1)"
+          lightColor="#eee"
+          style={styles.separator}
+        />
+        <EditScreenInfo path="app/(tabs)/index.tsx" />
+      </View>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
---
+++ apps/frontend/app/(tabs)/two.tsx
@@ -0,0 +1,99 @@
+import { AntDesign } from "@expo/vector-icons";
+import { PressableScale } from "pressto";
+import { useCallback, useRef } from "react";
+import { StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
+import { SwipeableCard } from "../../components/SwipeCards";
+import { IMAGES } from "../../constants/Images";
+import { useSwipeControls } from "../../hooks/use-swipe-controls";
+
+export default function SwipeCards() {
+  const { activeIndex, refs, swipeRight, swipeLeft, reset } =
+    useSwipeControls();
+
+  const liked = useRef(0);
+  const disliked = useRef(0);
+
+  const onReset = useCallback(() => {
+    activeIndex.value = 0;
+    liked.current = 0;
+    disliked.current = 0;
+
+    reset();
+  }, [activeIndex, reset]);
+
+  return (
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <View style={{ flex: 7 }}>
+          <Animated.View
+            entering={FadeIn}
+            exiting={FadeOut}
+            style={{
+              marginTop: 20,
+              justifyContent: "center",
+              alignItems: "center",
+              flex: 1,
+            }}
+          >
+            {new Array(IMAGES.length).fill(0).map((_, index) => (
+              <SwipeableCard
+                activeIndex={activeIndex}
+                image={IMAGES[index]}
+                index={index}
+                key={index}
+                onSwipeLeft={() => {
+                  disliked.current += 1;
+                }}
+                onSwipeRight={() => {
+                  liked.current += 1;
+                }}
+                ref={refs[index]}
+              />
+            ))}
+          </Animated.View>
+        </View>
+
+        {/* Define the buttons container */}
+        <View style={styles.buttonsContainer}>
+          <PressableScale onPress={swipeLeft} style={styles.button}>
+            <AntDesign color="white" name="close" size={32} />
+          </PressableScale>
+          <PressableScale
+            onPress={onReset}
+            style={[styles.button, { height: 60, marginHorizontal: 10 }]}
+          >
+            <AntDesign color="white" name="reload" size={24} />
+          </PressableScale>
+          <PressableScale onPress={swipeRight} style={styles.button}>
+            <AntDesign color="white" name="heart" size={32} />
+          </PressableScale>
+        </View>
+      </View>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    aspectRatio: 1,
+    backgroundColor: "#3A3D45",
+    borderRadius: 40,
+    boxShadow: "0px 4px 0px rgba(0, 0, 0, 0.1)",
+    height: 80,
+    justifyContent: "center",
+    marginHorizontal: 20,
+  },
+  buttonsContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+    flex: 2,
+    justifyContent: "center",
+  },
+  container: {
+    backgroundColor: "#242831",
+    flex: 1,
+  },
+});
